355290fc0df742fca18c06fd91d687598c4a5456
Shaun Marsh <shaunzillam@yahoo.com>
2017-04-24 03:24:58 -0600

    indentation corrections

--- LEDShow/animation_wipe.py
+++ LEDShow/animation_wipe.py
@@ -9,25 +9,26 @@ class AnimationWipe(Animation):
         self._running = False
 
     def run(self, leds):      
+
         """ Maps color range to distance and applies results to bisected strip """
-       self._running = True
+        self._running = True
 
-       #color range
-       x = int(self.controlrange(leds.distance, 1.0, 350, 0.0, 255.0))
+        #color range
+        x = int(self.controlrange(leds.distance, 1.0, 350, 0.0, 255.0))
 
-       #bottom section
-       for i in range(x):
+        #bottom section
+        for i in range(x):
            leds.setPixelColor(i, 24, x, 0)
            leds.show()
            time.sleep(21/500.0)
 
-       #top section
-       for i in range(256-x):
+        #top section
+        for i in range(256-x):
            leds.setPixelColor(i+x, 80, 0, x*2)
            leds.show()
            time.sleep(21/600.0)
 
-       self._running = False
+        self._running = False
 
     def running(self):
         """ Returns the running state of the animation """
@@ -39,11 +40,12 @@ class AnimationWipe(Animation):
 
     def	controlrange(self, OldValue, OldMin, OldMax, NewMin, NewMax, clamp = True):
         """ Maps color range to distance """
-	    NewValue = ( (OldValue - OldMin) / (OldMax - OldMin) ) * (NewMax - NewMin) + NewMin
+        newValue = ((OldValue - OldMin) /(OldMax - OldMin)) * (NewMax - NewMin) + NewMin
 
-	    if clamp == True:
-		    if NewValue < NewMin:
-			    NewValue = NewMin
-		    if NewValue > NewMax:
-			    NewValue = NewMax
-	    return NewValue
+        if clamp:
+            if(newValue < NewMin):
+                newValue = NewMin
+            if(newValue > NewMax):
+                newValue = NewMax
+
+        return newValue
--- LEDShow/main.py
+++ LEDShow/main.py
@@ -1,6 +1,8 @@
 from led_show import LEDShow
 
+#Entry point
 if __name__ == '__main__':
+    #Instantiate LED show
     LEDShow()
 
 
