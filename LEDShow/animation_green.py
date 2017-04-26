43052ce3b08ed0da30577c8a95c8e4c5babf1c69
Shaun Marsh <shaunzillam@yahoo.com>
2017-04-26 02:28:11 -0600

    ping accuracy test

--- LEDShow/animation_green.py
+++ LEDShow/animation_green.py
@@ -1,18 +1,22 @@
 from _animation import Animation
 import time
+import utils
 
 class AnimationGreen(Animation):
     """PLACEHOLDER ANIMATION"""
 
     def __init__(self):
+        self.row = -1
         self._running = False
 
     def run(self, leds):
        self._running = True
-       for i in range(leds.numPixels()):
-            leds.setPixelColor(i, 0, 255, 0)
-            leds.show()
-            time.sleep(21/500)
+       row = int(utils.mapRange(leds.distance, 80.0, 312.0, 31.0, 0.0))
+       if not(self.row == row):
+           leds.setRowColor(self.row, 0, 0, 0)
+           self.row = row
+           leds.setRowColor(self.row, 0, 255, 0)
+           leds.show()
        self._running = False
        
     def running(self):
